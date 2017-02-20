63743aa13e5da7cb0b6083572d204358afc5d4a7
olejorgensen <olejorgensen@me.com>
2017-02-20 18:24:41 +0100

    toggle the isWide css class when clicking image

--- app/components/rental-listing.js
+++ app/components/rental-listing.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // Since we want the image to be smaller at first, 
+  // we will set the property to start as false
+  isWide: false, 
+  actions: {
+    toggleImageSize() {
+      // toggle the isWide property
+      this.toggleProperty('isWide');
+    }
+  }
 });
