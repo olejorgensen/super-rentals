bc0298ba8289d5cc09984f7cd80f48df059734de
olejorgensen <olejorgensen@me.com>
2017-02-20 18:00:59 +0100

    test comments

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  beforeModel() {
+    // In our index route, we simply add the actual replaceWith invocation.
+    this.replaceWith('rentals');
+  }
 });
--- tests/unit/routes/index-test.js
+++ tests/unit/routes/index-test.js
@@ -4,6 +4,10 @@ moduleFor('route:index', 'Unit | Route | index');
 
 test('should transition to rentals route', function(assert) {
   let route = this.subject({
+    // A stub is simply a fake function that we provide to an object 
+    // we are testing, that takes the place of one that is already there.
+    // In this case we are stubbing the replaceWith function to assert 
+    // that it is called with what we expect.
     replaceWith(routeName) {
       assert.equal(routeName, 'rentals', 'replace with route name rentals');
     }
